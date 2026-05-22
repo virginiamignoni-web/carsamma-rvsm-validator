b257124c0ecb98571e33c11141ab172869dd2a17
virginiamignoni-web <virginiamignoni@gmail.com>
2026-05-22 08:27:01 -0300

    create: parsers index.ts exporting all parser modules

---
+++ scripts/parsers/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Parser Modules Index
+ * Central export point for all parser utilities
+ */
+
+export * from './csv-parser';
+export * from './tape-parser';
+export * from './mesh-parser';
+export * from './aircraft-parser';
